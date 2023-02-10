ef684f24e28ceae767905566fb673ff98eedc943
maxahirwe <max.ahirwe@gmail.com>
2023-02-10 22:04:25 +0200

    complete setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
 	def index
-		@prompt
+				@prompts = [
+  { language: "Ruby", prompt: "Write a method that takes in a string and returns the number of vowels in the string.", created_at:DateTime.now, response:'sample response'  },
+  { language: "JavaScript", prompt: "Create a function that takes in an array of numbers and returns the sum of all the even numbers in the array.", created_at:DateTime.now, response:'sample response' },
+  { language: "Python", prompt: "Write a function that takes in a string and returns the first non-repeated character in the string.", created_at:DateTime.now, response:'sample response' },
+  { language: "C++", prompt: "Create a program that reads in a list of integers and outputs the largest and smallest numbers in the list.", created_at:DateTime.now, response:'sample response' },
+  { language: "Ruby", prompt: "Write a method that takes in an array of integers and returns the median value of the array.", created_at:DateTime.now, response:'sample response' }
+]
+
+		@prompt = { language: 'Javascript' }
+		@promp
 		
 		render 'home/index'
 	end
